78ab4e5f6e1ecb10de53118edf8b50984a6adb17
reniohh <renohhreni@gmail.com>
2023-06-13 08:52:07 +0800

    melengkapi clas mahasiswa

--- src/universitas/mahasiswa.java
+++ src/universitas/mahasiswa.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universitas;
 
-/**
- *
- * @author USER
- */
 public class mahasiswa {
-    
+   String nama; 
+   int npm;
+   String prodi;
+   
+   public String getNama(){
+     return nama;  
+   }
+   public int getNpm(){
+     return npm; 
+   }
+   public String getProdi(){
+     return prodi; 
+   }
 }
+
